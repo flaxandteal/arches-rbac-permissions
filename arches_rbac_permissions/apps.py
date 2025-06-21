f8ca4975e224ef0130f6fd9b765b55655e22f54b
Phil Weir <phil.weir@flaxandteal.co.uk>
2025-06-21 16:48:45 +0100

    initial code

---
+++ arches_rbac_permissions/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ArchesRBACPermissionsConfig(AppConfig):
+    name = "arches_rbac_permissions"
+    is_arches_application = True
--- dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import re_path
-from dashboard.views.dashboard import Dashboard
-
-urlpatterns = [
-    re_path(r"^dashboard/", Dashboard.as_view(), name="dashboard"),
-]
